93cf8d1e1b8ccc90594b0a06c553db31e90bbae4
Shane McIntosh <shanemcintosh@acm.org>
2022-03-08 16:39:34 -0500

    Uwaterloo update

--- src/main/java/ca/uwaterloo/cs446/designpatterns/templatemethod/CrossCompiler.java
+++ src/main/java/ca/uwaterloo/cs446/designpatterns/templatemethod/CrossCompiler.java
@@ -1,4 +1,4 @@
-package ca.mcgill.ecse321.designpatterns.templatemethod;
+package ca.uwaterloo.cs446.designpatterns.templatemethod;
 
 public abstract class CrossCompiler {
 	public void crossCompile() {
